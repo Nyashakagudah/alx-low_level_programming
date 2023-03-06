c38b3fa72b7285f79f0962f61d4fec8b74433662
Nyashakagudah <kagudagrace@gmail.com>
2023-03-06 11:57:43 -0800

    4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,39 +1,28 @@
 #include "main.h"
 
 /**
- * *_strstr - Entry point
- * Description: Locates a substring
- * @haystack: Character
- * @needle: Character
+ * _strpbrk - Entry point
+ * Description: Searches a string for any of a set of bytes
+ * @s: Character
+ * @accept: Character
  * Return: char
  */
 
-char *_strstr(char *haystack, char *needle)
+char *_strpbrk(char *s, char *accept)
 {
-	/* If the needle is an empty string, return the haystack */
-	if (*needle == '\0')
+	while (*s != '\0')
 	{
-		return (haystack);
-	}
-	/* Loop through the haystack string */
-	while (*haystack)
-	{
-		char *h = haystack;
-		char *n = needle;
-		/* While the current characters of haystack and needle match */
-		while (*n != '\0' && *h == *n)
+		char *a = accept;
+
+		while (*a != '\0')
 		{
-			h++;
-			n++;
+			if (*a == *s)
+			{
+				return (s);
+			}
+			a++;
 		}
-		/* If we reached the end of the needle, we found a match */
-		if (*n == '\0')
-		{
-			return (haystack);
-		}
-		/* If not found, keep searching */
-		haystack++;
+		s++;
 	}
-	/* If we reached the end of the haystack and no match, return \0 */
 	return ('\0');
 }
